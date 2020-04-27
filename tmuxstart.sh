4c099bb2632aaab1896332e5c158cdb4be1709fc
mcharris <mcharris@adobe.com>
2020-04-26 20:23:44 -0600

    Add clearscreen and homedirectory behavior

--- tmuxstart.sh
+++ tmuxstart.sh
@@ -13,34 +13,49 @@ tmux start-server
 
 # create a new tmux session, starting vim from a saved session in the new window
 tmux new-session -d -s $session
+tmux send-keys "cd" C-m
+tmux send-keys "clear" C-m
 
 # Split pane 1 horizontal by 50%
 tmux splitw -h -p 50
 
 # Select pane 2 
-tmux selectp -t 2
+tmux selectp -t 1
+tmux send-keys "cd" C-m
+tmux send-keys "clear" C-m
 # Split pane 2 vertically by 50%
 tmux splitw -v -p 50
+tmux send-keys "cd" C-m
+tmux send-keys "clear" C-m
 
 # create a new window called adhoc
 tmux new-window -t $session:1 -n adhoc
+tmux send-keys "cd" C-m
+tmux send-keys "clear" C-m
 
 # create a new window called k8s
 tmux new-window -t $session:2 -n k8s
+tmux send-keys "cd" C-m
+tmux send-keys "clear" C-m
 
 # create a new window called repos
 tmux new-window -t $session:3 -n repos
 tmux send-keys "repos" C-m
+tmux send-keys "clear" C-m
 
 # create a new window called vim
 tmux new-window -t $session:4 -n vim
+tmux send-keys "cd" C-m
+tmux send-keys "clear" C-m
 
 # create a new window called pi
 tmux new-window -t $session:5 -n pi
 tmux send-keys "sshpi" C-m 
+tmux send-keys "clear" C-m
 
 # return to main vim window
 tmux select-window -t $session:0
+tmux selectp -t 0
 
 # Finished setup, attach to the tmux session!
 tmux attach-session -t $session
